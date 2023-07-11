cf09720872c07895333b2ca1e3d4ca2282c1a75f
Vladimir GYM Shcheglov <joepasqal@yandex.ru>
2023-07-11 19:04:50 +0500

    sdelal dz, есть вопрос по 60 задаче (почему то все выходит или в строку или в линию, а так все вроде стало ясно,когда двумя мозгами думать)

---
+++ task60/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] GenerateArray()
+{
+    int[,,] array = new int[2, 2, 2];
+    int num = 10;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                array[i, j, k] = num;
+                num++;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                Console.WriteLine($"{array[i, j, k]}({i},{j},{k})");
+            }
+        }
+    }
+}
+int[,,] array = GenerateArray();
+PrintArray(array);
